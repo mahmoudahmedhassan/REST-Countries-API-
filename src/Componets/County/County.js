9349321b87aeaf4262904034c776bc25f704398d
Mahmoud Ahmed <hekou77@gmail.com>
2022-01-19 15:20:36 +0200

    upload

--- src/Componets/County/County.js
+++ src/Componets/County/County.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
@@ -24,6 +25,7 @@ function County() {
 
   useEffect(() => {
     fetchCounty();
+    
      }, [])
 
   const data =
